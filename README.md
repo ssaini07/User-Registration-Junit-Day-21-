003704b2656f64103344fcb943ceaa90a87ad4d2
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-22 12:53:49 +0530

    Committing before testing cases

---
+++ README.md
@@ -0,0 +1,1 @@
+# User-Registration-Junit-Day-21-
---
+++ src/main/java/com/bridgelabz/UserRegistrationJunit/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.UserRegistrationJunit;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration");
+
+	}
+
+}
